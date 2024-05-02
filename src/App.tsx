dcdeff84f267d89a229b9cdf77d04d627e6086c5
yerko arce <yerko.arce.galaz@gmail.com>
2024-05-02 19:19:01 -0400

    completada al 90 por ciento la primera card

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,25 @@ function App() {
 
   return (
     <>
-      <h1 className=" font-rubik">Time tracking</h1>
+      
+      <section className="grid">
+        <article>
+          <div className=" grid-rows-2 bg-blue max-w-64 rounded-2xl p-10  ">
+            <img 
+              src=" ../public/images/image-jeremy.png" 
+              alt="Jeremy photo" 
+              className=" max-w-20 border-2 border-white rounded-full mb-8"
+            />
+            <h6 className=" text-soft-blue mb-2">Report for</h6>
+            <h1 className=" text-white text-5xl mb-10">Jeremy Robson</h1>
+          </div>
+          <div className=" bg-dark-blue max-w-64 -mt-4 p-10 flex flex-col gap-4">
+            <button className=" text-soft-blue text-start">Daily</button>
+            <button className="text-white text-start">Weekly</button>
+            <button className="text-soft-blue text-start">Monthly</button>
+          </div>
+        </article>
+      </section>
 
        
     </>
