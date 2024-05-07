392b9c6b28e2d0460887a4044bd524f9dde20559
yerko arce <yerko.arce.galaz@gmail.com>
2024-05-07 15:47:07 -0400

    ajustes para una mejor visualizacion del componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,36 @@
-import Card from "./components/Card"
-import { data } from "./data/data"
-
-
+import Card from "./components/Card";
+import { data } from "./data/data";
 
 function App() {
-  
-
   return (
     <>
-      
-      <section className="grid grid-rows-1 grid-cols-4">
-        <article>
-          <div className=" grid-rows-2 bg-blue max-w-64 rounded-2xl p-10  ">
-            <img 
-              src=" ../public/images/image-jeremy.png" 
-              alt="Jeremy photo" 
-              className=" max-w-20 border-2 border-white rounded-full mb-8"
-            />
-            <h6 className=" text-soft-blue mb-2">Report for</h6>
-            <h1 className=" text-white text-5xl mb-10">Jeremy Robson</h1>
+      <div className="">
+        <div className=" flex justify-center items-center h-full gap-4">
+          <article>
+            <div className=" grid-rows-2 bg-blue max-w-64 rounded-2xl p-10  ">
+              <img
+                src="/images/image-jeremy.png "
+                alt="Jeremy photo"
+                className=" max-w-20 border-2 border-white rounded-full mb-8"
+              />
+              <h6 className=" text-soft-blue mb-2">Report for</h6>
+              <h1 className=" text-white text-5xl mb-10">Jeremy Robson</h1>
+            </div>
+            <div className=" bg-dark-blue max-w-64 -mt-4 p-10 flex flex-col gap-4">
+              <button className=" text-soft-blue text-start">Daily</button>
+              <button className="text-white text-start">Weekly</button>
+              <button className="text-soft-blue text-start">Monthly</button>
+            </div>
+          </article>
+          <div className="grid grid-cols-3 gap-4 h-full">
+            {data.map((activity) => (
+              <Card key={activity.title} activity={activity} />
+            ))}
           </div>
-          <div className=" bg-dark-blue max-w-64 -mt-4 p-10 flex flex-col gap-4">
-            <button className=" text-soft-blue text-start">Daily</button>
-            <button className="text-white text-start">Weekly</button>
-            <button className="text-soft-blue text-start">Monthly</button>
-          </div>
-        </article>
-        <div>
-          {data.map(activity => (
-            <Card 
-              key={activity.title}
-              activity={activity}
-            />
-          ))}
         </div>
-      </section>
-
-       
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,10 +10,18 @@ export default function Card({ activity } : CardProps) {
   console.log(activity)
 
   return (
-    <div className=" bg-dark-blue m-w-64 text-white">
-      <h6>{activity.title}</h6>
-      <h1>{activity.timeframes.weekly.current}hrs</h1>
-      <p>Last week - {activity.timeframes.weekly.previous} hrs</p>
+    <div className=" bg-desaturated-blue rounded-3xl w-56 h-24">
+      <div className=" flex justify-around">
+        <div></div>
+        <img src={`../public/images/icon-${activity.title}.svg`} alt="image of card" className=" -mb-6 -mt-2 h-20" />
+      </div>
+      <div className=" bg-dark-blue m-w-64 text-white rounded-2xl p-4">
+        <h6 className=" py-3">{activity.title}</h6>
+        <h1 className=" text-5xl">{activity.timeframes.weekly.current}hrs</h1>
+        <p className=" text-sm text-soft-blue mt-3">Last week - {activity.timeframes.weekly.previous} hrs</p>
+        
+      </div>
     </div>
+    
   )
 }
